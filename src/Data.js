8dc4e8bccd6371a3af0c0e43fa4cf69d7ad37719
henrikalmquist <almquist.henrik@gmail.com>
2023-04-03 22:09:06 +0200

    x

--- src/Data.js
+++ src/Data.js
@@ -7,74 +7,102 @@ The two practices and the different perspective from the schools have formed a d
     `,
     images: [
         {
-            src: "img/Almquist_Casa-Mangabeiras-Exteriour.jpg",
-            text: `Early sketches for a refurbishment of a 60s concrete dream. Restructuring and redefining the spaces for a more open interaction inside as well as towards the outside. Ongoing commission. - Casa Mangaberias
+            src: "img/Almquist_Casa-Mangabeiras-01.jpg",
+            text: `2110 Casa Mangabeiras - Early sketches for a refurbishment of a 60s concrete dream. Restructuring and redefining the spaces for a more open interaction inside as well as towards the outside. Ongoing commission.
             
-[Click on the image ]`
+[Click on the image ]
+`
         },
         {
-            src: "img/Almquist_Follo-Museum-Extension_01.jpg",
-            text: `Organised along a non-heated gallery, the activities can spill out onto the courtyard, inviting the public as well as sharing knowledge between the different disciplines. Open competition entry Norway 2020. - Follo Museum Extension`
+            src: "img/Almquist_Casa-Mangabeiras-02.jpg",
+            text: `2110 Casa Mangabeiras - An excavated opening in the solid concrete wall offers a view from the meditation room onto the garden outside. The smooth white surfaces are contrasted by the organic form of the concrete.
+
+. | . . . . . . . . . . . .
+            `
         },
         {
-            src: "img/Almquist_Follo-Museum-Extension_02.jpg",
-            text: `Adding a new workshop atelier for several traditional crafts, Follo Museum hopes to further the visitor’s experience with practical exercise as well as gathering numerous associations under the same roof. In the program, the site integration and the flexible use of spaces was key points. - Follo Museum Extension`
+            src: "img/Almquist_Villa-Daempluckan-01.jpg",
+            text: `2309 Villa Dämpluckan - Lightly placed on the ground, the wooden structure takes in the nature around and permits people to live as much as outside as inside. Project done at Arrhov Frick Arkitektkontor.
+            
+. . | . . . . . . . . . . .
+            `
         },
         {
-            src: "img/Almquist_Publication-Kritik.jpg",
-            text: `Publication in Kritik Tidsskrift 2021 - The dominating principles of urban growth through densification, and its inherited political and economic processes, area put to trial as the industrial area of Sofielund, Malmö, manifests different qualities and responses to what a mixed and dynamic city could be. - Split City`
+            src: "img/Almquist_Follo-Museum-Extension-01.jpg",
+            text: `2010 Follo Museum Extension - Organised along a non-heated gallery, the activities can spill out onto the courtyard, inviting the public to take part in the buildings diverse program. Competition Norway 2020.
+            
+. . . | . . . . . . . . . .
+            `
         },
         {
-            src: "img/Almquist_Split-City.jpg",
-            text: `The task lies in interacting with what already exists and to propose a solution of continuity by integrating social values into the economic driven urban regeneration. By interlocking the two perspectives, a protocol driven design proposes new urban forms. - Split City`
+            src: "img/Almquist_Furniture-01.jpg",
+            text: `1707 Joining and Bending - Having two directions, the curve gestures the continuation of either the back- or armrest. The stool can be used by itself but also be placed next to another twin to form a bench. 
+            
+. . . . | . . . . . . . . .
+            `
         },
         {
-            src: "img/Almquist_A-Utopia-City-on-the-Shore-01.jpg",
-            text: `The ocean, from where all life derives, has been neglected. In a world of pollution and over consumption trash are piling up on our shores. In constructing a utopia based on a different view of what is sacred, the project pushes an ecological narrative of a society that lives and flourish in the ruin, on the consequences, of our society. - Sacred Matter`
+            src: "img/Almquist_Villa-Daempluckan-02.jpg",
+            text: `2309 Villa Dämpluckan - Placed on the high point of the island, sliding all open the window frames, an exquisite view can be enjoyed from all parts of the house. Project done at Arrhov Frick Arkitektkontor. 
+            
+. . . . . | . . . . . . . .
+            `
         },
         {
-            src: "img/Almquist_Sacred-Matter_02.jpg",
-            text: `Everything revolves around the harvesting and the revaluation of the found objects, at times instigating large infrastructural projects, at others, inspiring a direct dialogue with the horizon through a meditative practice. - Sacred Matter`
+            src: "img/Almquist_Follo-Museum-Extension-02.jpg",
+            text: `2010 Follo Museum Extension - Adding a new workshop atelier for several traditional crafts, Follo Museum hopes to expand  the visitor’s experience with practical exercise as well as gathering numerous associations under the same roof. 
+
+. . . . . . | . . . . . . .
+            `
         },
         {
-            src: "img/Almquist_Bending-and-Folding.jpg",
-            text: `Having two directions, the curve gestures the continuation of either the back- or armrest. The stool can be used by itself but also be placed next to another twin to form a bench. - Joining and Bending`
+            src: "img/Almquist_Brunnsparken-01.jpg",
+            text: `2209 Brunnsparken - The small kiosk activates the public space in opening its different shutters, giving protection for the passer by, a standing coffee or a seated meal. Competition Gothenburg 2022.
+
+. . . . . . . | . . . . . .
+            `
         },
         {
-            src: "img/Almquist_Casa-Mangabeiras-Interiour.jpg",
-            text: `An excavated opening in the solid concrete wall offers a view from the meditation room onto the garden outside. The smooth white surfaces are contrasted by the organic form of the concrete. - Casa Mangaberias`
+            src: "img/Almquist_Sofielund-02.jpg",
+            text: `2006 Sofielund - The dominating principles of urban growth through densification, and its inherited political and economic processes, area put to trial as the industrial area of Sofielund, Malmö, manifests different qualities and responses to what a mixed and dynamic city could be. Publication in Kritik Tidsskrift 2021.
+
+. . . . . . . . | . . . . .
+            `
         },
         {
-            src: "img/Almquist_Follo-Museum-Extension_03.jpg",
-            text: `Like the country road passes through the village, the visitors are led onto the inner courtyard framed by the new workshop extension and the exhibition building. A natural point of arrival and a space of events is defined for the large museum park of old houses moved here from different regions of Norway. - Follo Museum Extension`
-        },
-        {
-            src: "img/Almquist_Nothing-to-Hide.jpg",
-            text: `Taking the form of a simple and standardised letterbox, this one does, however, show the family (and all the neighbours) when new post has arrived. Why hide all your advertisement? - Nothing to hide`
-        },
-        {
-            src: "img/Almquist_A-Utopian-City-on-the-Shore-03.gif",
-            text: `Most people see it for what it is, a physical manifestation of the global ecological crises. Others see it as a material to understand and to take care of. In a future time, a separat society emerges along our coasts and the strong relation to the ocean gives place for new values and faiths. - Sacred Matter`
-        },
-        {
-            src: "img/Almquist_Structure-Japan-01.jpg",
-            text: `Interpreting the traditional carpentry and woodworking technics, the structures is simple, yet expressive. Non-built private commission. - Slide All Open`
+            src: "img/Almquist_Sofielund-01.jpg",
+            text: `2006 Sofielund - The task lies in interacting with what already exists and to propose a solution of continuity by integrating social values into the economic driven urban regeneration. By interlocking the two perspectives, a protocol driven design proposes new urban forms.
+
+. . . . . . . . . | . . . .
+            `
         },
         {
             src: "img/Almquist_Slide-All-Open.jpg",
-            text: `Three walls can be totally slid open and in the back an alcove, a tokonoma, defines the rear end of the space. The brick fundaments relate back to the surrounding landscape and seamlessly integrated into the wooden frames; mosquito nets assure a pleasant time all around the year. - Slide All Open`
+            text: `1908 Slide All Open - Three walls can be totally slid open towards the garden, making the space as much a building as a terrace. In the back an alcove, a tokonoma, defines the rear end of the space. Traditional tea house, ongoing commission. 
+
+. . . . . . . . . . | . . .
+            `
+        },
+        {
+            src: "img/Almquist_Furniture-02.jpg",
+            text: `1612 Nothing to Hide - Taking the form of a simple and standardised letterbox, this one does, however, show the family (and all the neighbours) when new post has arrived. Why hide all your advertisement?
+
+. . . . . . . . . . . | . .
+            `
         },
         {
             src: "img/Almquist_This-is-Public-Space.jpg",
-            text: `Inside, an art museum. Outside, as the exhibition of the public life in the city, a covered space with a smooth floor. Ideal for a dance. - This is Public Space`
-        },
-        {
-            src: "img/Almquist_City-Collage-Tokyo.jpg",
-            text: `The infrastructure through the valley, the high rise on one side and the lower and older buildings on the other. The beauty in strong contrast and the collage of a city changing with its time. Tokyo, of course. - Cityscape`
+            text: `2203 This is Public Space - Inside, an art museum. Outside, as the exhibition of the public life in the city, a covered space with a smooth floor. Ideal for a dance.
+
+. . . . . . . . . . . . | .
+            `
         },
         {
             src: "img/Almquist_Exhibiting-Space.jpg",
-            text: `Colours, shadows, reflections and expressive forms. The empty exhibition space put itself into the light, offering an atmosphere of suspense and a coming action. - Exhibiting space`
+            text: `2203 Exhibiting Space - Colours, shadows, reflections and expressive forms. The empty exhibition space put itself into the light, offering an atmosphere of suspense and a coming action. 
+
+. . . . . . . . . . . . . |
+            `
         },
     ],
 }
