36036b70978c1a56d36e008ea995df894ce1f2f9
henrikalmquist <almquist.henrik@gmail.com>
2023-04-05 21:54:50 +0200

    Create Data.js

---
+++ src/Data.js
@@ -0,0 +1,101 @@
+export default{
+    about: `Architecture is about the city, the collective practice of living together in a complex societal system. The city is about architecture, the small and large interventions of independent objects that constitute a rich and divers whole. Conceiving this, is a play with the contrast of a given situation and the dreams of its inhabitants, looking forward and backwards, proposing a functional strategy above functional.   
+
+Henrik Almquist is a Swedish Architect currently practicing in Stockholm at Arrhov Frick Arkitekter (2022-). He holds a Master of Art from ENSA Paris Malaquais and a Bachelor of Science from Chalmers Tekniska Högskola in Architecture and Engineering. The professional experiences, including the time in Paris at Studio Muoto (2020-2021) and Sou Fujimoto (2017-2019), has been an important training in how technical detailing and precise drawing in all phases are important to clearly formulate the intentions of the architectural vision. Saddling many seats - the economists, the structural engineers, the urbanist - the architect combines all the views into one coherent oeuvre in a real dialogue with its situation. 
+    
+The two practices and the different perspective from the schools have formed a dual approach on architecture and urbanism. An expanding theoretical conception is backed up by a technical knowledge and a thorough method in the formulation of strategies and the crafting of space. Taking into account what is already there, in terms of the place and the needs of the project, a situated architecture can be imagined and be given form. 
+    `,
+    images: [
+        {
+            src: "img/Almquist_Casa-Mangabeira-01.jpg",
+            text: `2110 Casa Mangabeira - Early sketches for a refurbishment of a 60s concrete dream. Restructuring and redefining the spaces for a more open interaction inside as well as towards the outside. Ongoing commission.
+            
+[Click on the image ]
+`
+        },
+        {
+            src: "img/Almquist_Casa-Mangabeira-02.jpg",
+            text: `2110 Casa Mangabeira - An excavated opening in the solid concrete wall offers a view from the meditation room onto the garden outside. The smooth white surfaces are contrasted by the organic form of the concrete.
+
+. | . . . . . . . . . . .
+            `
+        },
+        {
+            src: "img/Almquist_Follo-Museum-Extension-01.jpg",
+            text: `2010 Follo Museum Extension - Organised along a non-heated gallery, the activities can spill out onto the courtyard, inviting the public to take part in the buildings diverse program. Competition Norway 2020.
+            
+. . | . . . . . . . . . .
+            `
+        },
+        {
+            src: "img/Almquist_Furniture-01.jpg",
+            text: `1707 Joining and Bending - Having two directions, the curve gestures the continuation of either the back- or armrest. The stool can be used by itself but also be placed next to another twin to form a bench. 
+            
+. . . | . . . . . . . . .
+            `
+        },
+        {
+            src: "img/Almquist_Casa-Mangabeira-03.jpg",
+            text: `2110 Casa Mangabeira - Liberating qualities and opening up the inside with new windows, a former carport is also integrated to create space for a meditation room. The painted concrete bricks mark a contrast between new and old, creating a playful new expression for the street façade. 
+            
+. . . . | . . . . . . . .
+            `
+        },
+        {
+            src: "img/Almquist_Follo-Museum-Extension-02.jpg",
+            text: `2010 Follo Museum Extension - Adding a new workshop atelier for several traditional crafts, Follo Museum hopes to expand  the visitor’s experience with practical exercise as well as gathering numerous associations under the same roof. 
+
+. . . . . | . . . . . . .
+            `
+        },
+        {
+            src: "img/Almquist_Brunnsparken-01.jpg",
+            text: `2209 Brunnsparken - The small kiosk activates the public space in opening its different shutters, giving protection for the passer by, a standing coffee or a seated meal. Competition Gothenburg 2022.
+
+. . . . . . | . . . . . .
+            `
+        },
+        {
+            src: "img/Almquist_Sofielund-02.jpg",
+            text: `2006 Sofielund - The dominating principles of urban growth through densification, and its inherited political and economic processes, area put to trial as the industrial area of Sofielund, Malmö, manifests different qualities and responses to what a mixed and dynamic city could be. Publication in Kritik Tidsskrift 2021.
+
+. . . . . . . | . . . . .
+            `
+        },
+        {
+            src: "img/Almquist_Sofielund-01.jpg",
+            text: `2006 Sofielund - The task lies in interacting with what already exists and to propose a solution of continuity by integrating social values into the economic driven urban regeneration. By interlocking the two perspectives, a protocol driven design proposes new urban forms.
+
+. . . . . . . . | . . . .
+            `
+        },
+        {
+            src: "img/Almquist_Slide-All-Open.jpg",
+            text: `1908 Slide All Open - Three walls can be totally slid open towards the garden, making the space as much a building as a terrace. In the back an alcove, a tokonoma, defines the rear end of the space. Traditional tea house, private commission. 
+
+. . . . . . . . . | . . .
+            `
+        },
+        {
+            src: "img/Almquist_Furniture-02.jpg",
+            text: `1612 Nothing to Hide - Taking the form of a simple and standardised letterbox, this one does, however, show the family (and all the neighbours) when new post has arrived. Why hide all your advertisement?
+
+. . . . . . . . . . | . .
+            `
+        },
+        {
+            src: "img/Almquist_This-is-Public-Space.jpg",
+            text: `2203 This is Public Space - Inside, an art museum. Outside, as the exhibition of the public life in the city, a covered space with a smooth floor. Ideal for a dance.
+
+. . . . . . . . . . . | .
+            `
+        },
+        {
+            src: "img/Almquist_Exhibiting-Space.jpg",
+            text: `2203 Exhibiting Space - Colours, shadows, reflections and expressive forms. The empty exhibition space put itself into the light, offering an atmosphere of suspense and a coming action. 
+
+. . . . . . . . . . . . |
+            `
+        },
+    ],
+}
