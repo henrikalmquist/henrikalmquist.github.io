4e0a2c8976ec6030501902ab2390d0c2c1923eb9
henrikalmquist <almquist.henrik@gmail.com>
2023-04-05 21:55:10 +0200

    Updates

--- assets/index-b2704919.js
+++ assets/index-b2704919.js
@@ -24,41 +24,38 @@ The two practices and the different perspective from the schools have formed a d
 [Click on the image ]
 `},{src:"img/Almquist_Casa-Mangabeira-02.jpg",text:`2110 Casa Mangabeira - An excavated opening in the solid concrete wall offers a view from the meditation room onto the garden outside. The smooth white surfaces are contrasted by the organic form of the concrete.
 
-. | . . . . . . . . . . . .
-            `},{src:"img/Almquist_Villa-Daempluckan-01.jpg",text:`2209 Villa Dämpluckan - Lightly placed on the ground, the wooden structure takes in the nature around and permits people to live as much as outside as inside. Project done at Arrhov Frick Arkitektkontor.
-            
-. . | . . . . . . . . . . .
+. | . . . . . . . . . . .
             `},{src:"img/Almquist_Follo-Museum-Extension-01.jpg",text:`2010 Follo Museum Extension - Organised along a non-heated gallery, the activities can spill out onto the courtyard, inviting the public to take part in the buildings diverse program. Competition Norway 2020.
             
-. . . | . . . . . . . . . .
+. . | . . . . . . . . . .
             `},{src:"img/Almquist_Furniture-01.jpg",text:`1707 Joining and Bending - Having two directions, the curve gestures the continuation of either the back- or armrest. The stool can be used by itself but also be placed next to another twin to form a bench. 
             
-. . . . | . . . . . . . . .
-            `},{src:"img/Almquist_Villa-Daempluckan-02.jpg",text:`2209 Villa Dämpluckan - Placed on the high point of the island, sliding all open the window frames, an exquisite view can be enjoyed from all parts of the house. Project done at Arrhov Frick Arkitektkontor. 
+. . . | . . . . . . . . .
+            `},{src:"img/Almquist_Casa-Mangabeira-03.jpg",text:`2110 Casa Mangabeira - Liberating qualities and opening up the inside with new windows, a former carport is also integrated to create space for a meditation room. The painted concrete bricks mark a contrast between new and old, creating a playful new expression for the street façade. 
             
-. . . . . | . . . . . . . .
+. . . . | . . . . . . . .
             `},{src:"img/Almquist_Follo-Museum-Extension-02.jpg",text:`2010 Follo Museum Extension - Adding a new workshop atelier for several traditional crafts, Follo Museum hopes to expand  the visitor’s experience with practical exercise as well as gathering numerous associations under the same roof. 
 
-. . . . . . | . . . . . . .
+. . . . . | . . . . . . .
             `},{src:"img/Almquist_Brunnsparken-01.jpg",text:`2209 Brunnsparken - The small kiosk activates the public space in opening its different shutters, giving protection for the passer by, a standing coffee or a seated meal. Competition Gothenburg 2022.
 
-. . . . . . . | . . . . . .
+. . . . . . | . . . . . .
             `},{src:"img/Almquist_Sofielund-02.jpg",text:`2006 Sofielund - The dominating principles of urban growth through densification, and its inherited political and economic processes, area put to trial as the industrial area of Sofielund, Malmö, manifests different qualities and responses to what a mixed and dynamic city could be. Publication in Kritik Tidsskrift 2021.
 
-. . . . . . . . | . . . . .
+. . . . . . . | . . . . .
             `},{src:"img/Almquist_Sofielund-01.jpg",text:`2006 Sofielund - The task lies in interacting with what already exists and to propose a solution of continuity by integrating social values into the economic driven urban regeneration. By interlocking the two perspectives, a protocol driven design proposes new urban forms.
 
-. . . . . . . . . | . . . .
+. . . . . . . . | . . . .
             `},{src:"img/Almquist_Slide-All-Open.jpg",text:`1908 Slide All Open - Three walls can be totally slid open towards the garden, making the space as much a building as a terrace. In the back an alcove, a tokonoma, defines the rear end of the space. Traditional tea house, private commission. 
 
-. . . . . . . . . . | . . .
+. . . . . . . . . | . . .
             `},{src:"img/Almquist_Furniture-02.jpg",text:`1612 Nothing to Hide - Taking the form of a simple and standardised letterbox, this one does, however, show the family (and all the neighbours) when new post has arrived. Why hide all your advertisement?
 
-. . . . . . . . . . . | . .
+. . . . . . . . . . | . .
             `},{src:"img/Almquist_This-is-Public-Space.jpg",text:`2203 This is Public Space - Inside, an art museum. Outside, as the exhibition of the public life in the city, a covered space with a smooth floor. Ideal for a dance.
 
-. . . . . . . . . . . . | .
+. . . . . . . . . . . | .
             `},{src:"img/Almquist_Exhibiting-Space.jpg",text:`2203 Exhibiting Space - Colours, shadows, reflections and expressive forms. The empty exhibition space put itself into the light, offering an atmosphere of suspense and a coming action. 
 
-. . . . . . . . . . . . . |
+. . . . . . . . . . . . |
             `}]};const g2e={data(){return{imagesVisible:!0,images:Ys.images,description:Ys.about,cvLcoation:"almquist-cv.pdf",mail:"henrik@almquist.eu",instagram:"https://www.instagram.com/almquisthenrik/"}},computed:{imagesVisibleComputed(){return this.imagesVisible}},methods:{introClicked(){this.imagesVisible=!this.imagesVisible}},components:{Header:b2,ContactInformation:nb,ImagesContainer:k2e,AboutText:Wi}},_2e={class:"flex flex-col h-screen justify-between font-stratford"},v2e={class:"container mb-16 md:mb-auto mx-auto flex flex-col items-center h-5/6 max-w-6xl pt-5 md:pt-10"},w2e=$e("h1",{class:"text-5xl"},[$e("a",{href:"/"},"Almquist")],-1);function x2e(t,e,n,o,r,c){const s=He("ContactInformation"),l=He("images-container"),i=He("about-text");return ve(),it("div",_2e,[$e("div",v2e,[w2e,te(s,{class:"h-10 mb-2","cv-location":r.cvLcoation,mail:r.mail,instagram:r.instagram,"images-visible":c.imagesVisibleComputed,onIntroClicked:c.introClicked},null,8,["cv-location","mail","instagram","images-visible","onIntroClicked"]),mr(te(l,{images:r.images,description:r.description,class:"h-full md:h-2/3"},null,8,["images","description"]),[[lo,c.imagesVisibleComputed]]),mr(te(i,{description:r.description,class:"h-full md:h-2/3"},null,8,["description"]),[[lo,!c.imagesVisibleComputed]])])])}const E2e=Yt(g2e,[["render",x2e]]);Di(E2e).mount("#app");
