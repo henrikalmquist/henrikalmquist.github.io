9a5f8110ba3defc057d5ac4c5476822f4297a5a6
henrikalmquist <90309066+henrikalmquist@users.noreply.github.com>
2025-11-16 22:33:47 +0100

    ha

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  base: './',   // relative paths for github pages
+  build: {
+    outDir: 'dist'  // default, keep as dist unless you prefer docs
+  }
 })
