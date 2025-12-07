6a828ad298203af899c11cb59254eaed501a7c85
henrikalmquist <90309066+henrikalmquist@users.noreply.github.com>
2025-12-07 17:41:39 +0100

    ha

--- src/pages/Agency.vue
+++ src/pages/Agency.vue
@@ -5,6 +5,11 @@
       <!-- Part one -->
       <div class="intro" v-html="renderMarkdown(currentText(agency.partOne))"></div>
 
+
+
+      <!-- Part two -->
+      <div class="rest2" v-if="currentText(agency.partTwo)" v-html="renderMarkdown(currentText(agency.partTwo))">  </div>
+
       <!-- PROFILE SECTION -->
       <ImagesContainerCollapsed
         v-for="(p, i) in profileForLang"
@@ -15,11 +20,6 @@
         :showonload="p.showonload"
         :rem-unit="remUnit"
       />
-
-      <!-- Part two -->
-      <div class="rest2" v-if="currentText(agency.partTwo)" v-html="renderMarkdown(currentText(agency.partTwo))">  </div>
-
-
 
 
      
