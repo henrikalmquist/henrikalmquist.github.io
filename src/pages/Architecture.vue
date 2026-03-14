f9dfa7946f8e9df6587ef1a3854275801f981f89
henrikalmquist <90309066+henrikalmquist@users.noreply.github.com>
2026-03-14 17:27:41 +0100

    push 5

--- src/pages/Architecture.vue
+++ src/pages/Architecture.vue
@@ -80,7 +80,7 @@ AAA works with renovations, additions and strategies for the existing environmen
 <div class="service-line">
   <div class="service-title"><strong>Consultancy</strong></div>
   <div class="service-desc">
-    <a href="#/adaptive" class="blink-link">Unlocking existing values</a>
+    <a href="/#/adaptive" class="blink-link">Unlocking existing values</a>
    in early stage phases and providing strategic and practical decision support for municipalities and developers.
   </div>
 </div>
