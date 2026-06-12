637e7c804d0f516332243bbfe982b0ec5677d975
henrikalmquist <90309066+henrikalmquist@users.noreply.github.com>
2026-06-12 14:59:06 +0200

    Fix HA

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import './index.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+
 createApp(App).use(router).mount('#app');
+
+router.afterEach((to) => {
+  if (window.goatcounter && window.goatcounter.count) {
+    window.goatcounter.count({
+      path: to.path,
+      title: document.title,
+    });
+  }
+});
